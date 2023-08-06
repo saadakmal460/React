0daf111e88a8598b832f397a8e4c01c3dfe7b20f
Saad@DESKTOP-UBGD9AO <saadakmal460@gmail.comgit config --global user.email saadakmal460@gmail.com>
2023-08-06 13:26:09 +0500

    Project2

---
+++ project2/src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./Components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <ul>
+        <li>Hello</li>
+        <li>Hello</li>
+        <li>Hello</li>
+      </ul>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
